cbad9a94cf3cfea676f4b2f9024d312ef7cc06b4
ltrzalez <ltrzalez@gmail.com>
2021-07-01 01:23:16 -0300

    maybe now is ok

---
+++ public/logFunction.js
@@ -0,0 +1,26 @@
+const auth = firebase.auth()
+
+const noLogged = document.getElementById('noLogged')
+const loggedIn = document.getElementById('loggedIn')
+const singInButton = document.getElementById('singInButton')
+const singOutButton = document.getElementById('singOutButton')
+const userDetails = document.getElementById('userDetails')
+
+const provider = new firebase.auth.GoogleAuthProvider()
+
+singInButton.onclick = () => {
+    try {
+        auth.signInWithPopup(provider)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+singOutButton.onclick = () => {
+    try {
+        auth.singOut()
+    } catch (error) {
+        console.log(error)
+    }
+
+}
