3366b1de9603e3a6909bd360486f5d0d78f4674c
ltrzalez <ltrzalez@gmail.com>
2021-07-01 00:25:07 -0300

    this is a constructor?

--- public/authProvider.js
+++ public/authProvider.js
@@ -6,7 +6,7 @@ const singInButton = document.getElementById('singInButton')
 const singOutButton = document.getElementById('singOutButton')
 const userDetails = document.getElementById('userDetails')
 
-const provider = new firebase.auth.GoogleAuthprovider()
+const provider = firebase.auth.GoogleAuthprovider()
 
 singInButton.onclick = () => {
     try {
