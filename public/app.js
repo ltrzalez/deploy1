fa6640b053a50b04e9ed421f5cea3a378b36d00a
ltrzalez <ltrzalez@gmail.com>
2021-07-01 00:14:45 -0300

    sing in with google

--- public/app.js
+++ public/app.js
@@ -1,1 +1,13 @@
-console.log('me gusta aprender. Y a vos?')
+const auth = firebase.auth()
+
+const noLogged = document.getElementById('noLogged')
+const loggedIn = document.getElementById('loggedIn')
+const singInButton = document.getElementById('singInButton')
+const singOutButton = document.getElementById('singOutButton')
+const userDetails = document.getElementById('userDetails')
+
+const provider = new firebase.auth.GoogleAuthprovider()
+
+singInButton.onclick = () => auth.signInWithPopup(provider)
+
+singOutButton.onclick = () => auth.singOut()
